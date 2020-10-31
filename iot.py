a6345bd10d3fb1f75e834cc53318c1babcc2d2c9
CamCommand <48034712+CamCommand@users.noreply.github.com>
2020-10-31 14:16:31 -0400

    Add files via upload

--- iot.py
+++ iot.py
@@ -1,7 +1,10 @@
 import webbrowser
-import time
+import serial
+import keyboard
+import sys
 
 new=2
-
+keyboard.press_and_release('win + m')#command to close all windows in Windows
+keyboard.press_and_release('option + h + m')#command to close all windows in macOS
 url="https://www.arduino.cc/en/main/docs"
 webbrowser.open(url,new=new)
