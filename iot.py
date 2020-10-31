a255bcbd613a8bbfce32b79bce3b5cc6ce2a8338
CamCommand <48034712+CamCommand@users.noreply.github.com>
2020-10-31 14:27:42 -0400

    added before the storm
    this minimizes all the windows you don't want seen and opens documentation very quickly to make it look like you are working

--- iot.py
+++ iot.py
@@ -6,5 +6,9 @@ import sys
 new=2
 keyboard.press_and_release('win + m')#command to close all windows in Windows
 keyboard.press_and_release('option + h + m')#command to close all windows in macOS
-url="https://www.arduino.cc/en/main/docs"
+url="https://www.arduino.cc/en/main/docs"# to make you look busy
+url2="https://docs.python.org/3/library/index.html"# extra busy with python
+url3="https://www.arduino.cc/reference/en/"# extra referenece
 webbrowser.open(url,new=new)
+webbrowser.open(url2,new=new)
+webbrowser.open(url3,new=new)
